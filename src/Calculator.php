f446a8a55bd8a8b5ba6ac1de34832cd417138543
William-lh <lherm.william@gmail.com>
2023-12-12 22:31:52 +0100

    phpunit

--- src/Calculator.php
+++ src/Calculator.php
@@ -8,4 +8,19 @@ class Calculator
     {
         return $firstValue + $secondValue;
     }
+
+    public function multiplication(int $valueOne, int $valueTwo): int
+    {
+        return $valueOne * $valueTwo;
+    }
+
+    public function isEven(int $paramBool)
+    {
+        if($paramBool % 2 === 0)
+        {
+            return true;
+        } else {
+            return false;
+        }        
+    }
 }
--- tests/CalculatorTest.php
+++ tests/CalculatorTest.php
@@ -22,4 +22,37 @@ class CalculatorTest extends TestCase
             "La méthode sum() ne fonctionne plus :-("
         );
     }
+
+    public function testMultiplication(): void
+    {
+        //Arrange
+        $valueOne = 5;
+        $valueTwo = 8;
+        $expectedResult = 40;
+
+        //Act
+        $calculator = new Calculator();
+        $multiplication = $calculator->multiplication($valueOne, $valueTwo);
+        
+        //Assert
+        $this->assertSame($expectedResult, $multiplication, 
+            "La méthode multiplication() ne fonctionne plus :-("
+        );
+    }
+
+    public function testIsEven(): void
+    {
+        //Arrange
+        $paramBool = 58;        
+        $expectedResult = true;
+
+        //Act
+        $calculator = new Calculator();
+        $isEven = $calculator->isEven($paramBool);
+        
+        //Assert
+        $this->assertSame($expectedResult, $isEven, 
+            "La méthode isEven() ne fonctionne plus :-("
+        );
+    }
 }
